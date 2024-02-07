93754680873e1ccbc35c6e4cb07725911151c32f
FallThunder <pratyush.siva@gmail.com>
2024-02-07 10:38:25 -0500

    Spun up repo to share with Mush

---
+++ Utilities.py
@@ -0,0 +1,75 @@
+import random
+
+def dealCards():
+    """
+    Returns two lists of 26 tuples representing a deck of cards
+    
+    Parameters:
+    - None
+    
+    Returns:
+    - p1_hand: list of 26 tuples
+    - p2_hand: list of 26 tuples
+    """
+    # Define the cards in a deck
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    # Define the suites in a deck
+    suits = ['spades', 'diamonds', 'clubs', 'hearts']
+
+    # Create a deck of cards
+    deck = [(rank, suit) for rank in ranks for suit in suits]
+
+    # Initialize the hands for the two players
+    p1_hand = []
+    p2_hand = []
+
+    # Shuffle the deck using the random module
+    random.shuffle(deck)
+
+    # Deal the cards to the two players
+    p1_hand = deck[:26]
+    p2_hand = deck[26:]
+
+    # Return the hands
+    return p1_hand, p2_hand
+
+def playRound(p1_card, p2_card):
+    """
+    Returns the result of a round of war
+    
+    Parameters:
+    - p1_card: string
+    - p2_card: string
+    
+    Returns:
+    - -1: if player 1 wins
+    - 1: if player 2 wins
+    - None: if the round is a tie
+    """
+    # Define the ranks of the cards
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    # Compare the ranks of the cards
+    if ranks.index(p1_card) > ranks.index(p2_card):
+        return -1
+    
+    elif ranks.index(p1_card) < ranks.index(p2_card):
+        return 1
+
+def cardsCheck(p1_hand, p2_hand):
+    """
+    Returns True if both players have cards to play
+    
+    Parameters:
+    - p1_hand: list of tuples
+    - p2_hand: list of tuples
+    
+    Returns:
+    - True: if both players have cards to play
+    - False: if one or both players are out of cards
+    """
+    # Check if the players have cards to play
+    if len(p1_hand) == 0 or len(p2_hand) == 0:
+        return False
+    else:
+        return True
---
+++ main.py
@@ -0,0 +1,117 @@
+from Utilities import dealCards
+from Utilities import playRound
+from Utilities import cardsCheck
+import random
+import os
+import sys
+
+def main():
+    # Clear the terminal
+    os.system('clear')
+    # Initialize the play again variable
+    play_again = True
+
+    print("Welcome to War!\n")
+
+    # Create a score tracker for total games played and the number of wins for each player
+    p1_score = 0
+    p2_score = 0
+
+    # Begin the game loop
+    while play_again:
+        print("Shuffling...\n")
+
+        # Deal the cards to the two players
+        p1_hand, p2_hand = dealCards()
+
+        print("Lets begin!\n")
+
+        # Initialize the round number
+        round_num = 1
+
+        # Begin the round loop, where the game is played until one player runs out of cards
+        while len(p1_hand) > 0 and len(p2_hand) > 0:
+            # Initialize the round variables
+            won_round = False
+            p1_played, p2_played = [], []
+
+            print(f"Round {round_num}:")
+
+            # Begin the war loop, where the round is played until one player card trumps the others
+            while not won_round:
+                # Check if the players have cards to play
+                if not cardsCheck(p1_hand, p2_hand):
+                    # If one player is out of cards, the other player wins
+                    if len(p1_hand) == 0:
+                        print("Player 1 is out of cards! Player 2 wins!\n")
+                        p2_score += 1
+                        break
+
+                    else:
+                        print("Player 2 is out of cards! Player 1 wins!\n")
+                        p1_score += 1
+                        break
+
+                else:
+                    # Play the top card from each players hand
+                    p1_played.append(p1_hand.pop(0))
+                    p2_played.append(p2_hand.pop(0))
+
+                print(f"Player 1 plays {p1_played[-1][0]} of {p1_played[-1][-1]}\n")
+                print(f"Player 2 plays {p2_played[-1][0]} of {p2_played[-1][-1]}\n")
+
+                # Determine the winner of the round
+                result = playRound(p1_played[-1][0], p2_played[-1][0])
+
+                # Handles the result of playRound
+                if result == -1 or result == 1:
+                    # Add the played cards to a temporary deck to be shuffled
+                    added_hand = p1_played + p2_played
+                    # Shuffle the cards
+                    random.shuffle(added_hand)
+                    # Add the shuffled cards to the winning players hand
+                    if result == -1:
+                        p1_hand.extend(added_hand)
+                        print(f"Player 1 wins the round, with a new card count of {len(p1_hand)} cards!\n")
+                        won_round = True
+
+                    else:
+                        p2_hand.extend(added_hand)
+                        print(f"Player 2 wins the round, with a new card count of {len(p2_hand)} cards!\n")
+                        won_round = True
+
+                else:
+                    continue
+
+            # Increment the round number
+            round_num += 1
+
+        # Determine the winner of the game
+        if len(p1_hand) > len(p2_hand):
+            print(f"Player 1 wins the game in {round_num} rounds!\n")
+            p1_score += 1
+
+        else:
+            print(f"Player 2 wins the game in {round_num} rounds!\n")
+            p2_score += 1
+
+        # Print the current score
+        if p1_score > p2_score:
+            print(f"Player 1 leads with {p1_score} wins to Player 2 with {p2_score} wins.\n")
+
+        elif p1_score < p2_score:
+            print(f"Player 2 leads with {p2_score} wins to Player 1 with {p1_score} wins.\n")
+
+        else:
+            print(f"Player 1 and Player 2 are tied with {p1_score} wins each.\n")
+
+        print(f"{p1_score + p2_score} game(s) have been played.\n")
+
+        print("Would you like to play again? (return/n)")
+
+        # Check if the user wants to play again
+        if input() == 'n':
+            sys.exit()
+
+if __name__ == "__main__":
+    main()
